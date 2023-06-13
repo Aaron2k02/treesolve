6830b73f4cf07d70d11fddf5a50f1562e33fbd0b
Aaron Chee <u2102810@siswa.um.edu.my>
2023-06-13 21:35:07 +0800

    changes on news letter database

--- src/sql/nature_data.sql
+++ src/sql/nature_data.sql
@@ -48,6 +48,28 @@ CREATE TABLE IF NOT EXISTS `natureData`.`newsLetterfaqs` (
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
+-- Table `natureData`.`newsLetterRegistration`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `natureData`.`registration` (
+  `registration_id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `message` TEXT NOT NULL,
+  PRIMARY KEY (`registration_id`)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `natureData`.`News Letter Registration Faqs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `natureData`.`newsLetterfaqs` (
+  `faq_id` INT NOT NULL AUTO_INCREMENT,
+  `question` VARCHAR(255) NOT NULL,
+  `answer` TEXT NOT NULL,
+  PRIMARY KEY (`faq_id`)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
 -- Table `natureData`.`tree`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `natureData`.`tree` (
