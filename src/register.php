35ef0d0ba7ebf432c8feda3c9fc82dd67117c128
TanChunHong <tanchunhong717@gmail.com>
2023-06-18 16:19:05 +0800

    Update register.php

--- src/register.php
+++ src/register.php
@@ -113,7 +113,7 @@
                     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
                     // Prepare and execute the query
-                    $query = "INSERT INTO user (`firstname`, `lastname`, `email`, `phonenumber`, `password`) VALUES ('$firstname', '$lastname', '$email', '$phonenumber', '$hashedPassword')";
+                    $query = "INSERT INTO user (first_name, last_name, email, contact_number, `password`, in_mailing_list) VALUES ('$firstname', '$lastname', '$email', '$phonenumber', '$hashedPassword', 0)";
                     $result = mysqli_query($mysqli, $query);
 
                     if ($result) {
